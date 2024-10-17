f44e5abc858edc6392b960d3418a6e6e1cf0f3e2
Gustavip <148286101+Gustavip@users.noreply.github.com>
2024-10-17 14:19:04 -0600

    Paso 3
    creamos una vista de nuestros datos en el controlador

---
+++ practicaRepaso/app/Http/Controllers/RepasoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RepasoController extends Controller
+{
+    
+    public function index()
+{
+    return view('repaso1');
+}
+
+}
+
--- practicaRepaso/routes/web.php
+++ practicaRepaso/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/repaso1', [App\Http\Controllers\RepasoController::class, 'index'])->name('repaso1');
