41c328849d4be6d17c312f1a31a698335cb72904
Gustavip <148286101+Gustavip@users.noreply.github.com>
2024-10-30 09:43:56 -0600

    creacion de rutas

--- practicaRepaso2/routes/web.php
+++ practicaRepaso2/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [LibroController::class, 'inicio'])->name('principal');
+Route::get('/registro', [LibroController::class, 'crear'])->name('registro_libro');
+Route::post('/registro', [LibroController::class, 'almacenar'])->name('libro.almacenar');
