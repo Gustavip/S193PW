213672b7297f3c774c86694a4dabdfb7be109094
Gustavip <148286101+Gustavip@users.noreply.github.com>
2024-11-05 19:50:57 -0600

    primer commit

---
+++ Examen2/resources/views/vistaRegistro.blade.php
@@ -0,0 +1,33 @@
+<! DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio</title>
+</head>
+    <div class="card-header fs-5 text-primari>
+    <form>
+                    <div class="mb-3">
+                        <label for="prenda" class="form-label">prenda:</label>
+                        <input type="textprenda" class="form-control" id="prenda">
+                    </div>
+                    <div class="mb-3">
+                        <label for="color" class="form-label">color:</label>
+                        <input type="textcolor" class="form-control" id="apellido">
+                    </div>
+                    <div class="mb-3">
+                        <label for="cantidad" class="form-label">cantidad:</label>
+                        <input type="txtcantidad" class="form-control" id="cantidad">
+
+                        </div>
+            </div>
+            <div class="card-footer text-muted">
+                <div class="d-grid gap-2 mt-2 mb-1">
+                    <button type="submit" class="btn btn-success btn-sm">Guardar prenda</button>
+                </div>
+                </form> 
+
+                </html> 
+
+
+    
